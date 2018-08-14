8ac2ebf69c11533039a81db9505400c531ad5339
Pavel Lobashov <shockwavenn@gmail.com>
2018-08-14 15:15:21 +0300

    Use local.json file with custom config defined via jq

--- after-run.sh
+++ after-run.sh
@@ -1,8 +1,10 @@
-docker cp ~/local.json doc-linux:/etc/onlyoffice/documentserver/local.json
-docker cp ~/config.js doc-linux:/var/www/onlyoffice/documentserver/server/Metrics/config/config.js
 echo 'Sleep for 30 seconds to wait until documentserver reinitialize himself'
 sleep 30
-docker exec -it doc-linux sed -i '/"sessionidle": "0",/c\"sessionidle": "1h",' /etc/onlyoffice/documentserver/default.json
+
+docker exec -it doc-linux bash -c "apt-get -y update && apt-get -y install jq"
+docker exec -it doc-linux bash -c "jq '.services.CoAuthoring.expire.sessionidle=\"1h\"' /etc/onlyoffice/documentserver/local.json > /etc/onlyoffice/documentserver/local.custom"
+docker exec -it doc-linux cp /etc/onlyoffice/documentserver/local.custom /etc/onlyoffice/documentserver/local.json
+
 docker exec -it doc-linux sed -i 's/WARN/ALL/g' /etc/onlyoffice/documentserver/log4js/production.json
 docker exec -it doc-linux supervisorctl restart all
 docker cp archive-share-libs.sh doc-linux:/tmp/archive-share-libs.sh

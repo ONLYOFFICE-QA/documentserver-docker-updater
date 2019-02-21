c13709117cd1e9b20351797709588ad315a4cb8a
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-02-21 18:19:55 +0300

    New name for config of supervisor (#22)
    Since https://github.com/ONLYOFFICE/document-server-package/commit/902a2d2f643ba3232d400dd707ec650c739fd6e8

--- after-run.sh
+++ after-run.sh
@@ -5,7 +5,7 @@ docker exec -it doc-linux /var/www/onlyoffice/documentserver/npm/node_modules/.b
 docker exec -it doc-linux /var/www/onlyoffice/documentserver/npm/node_modules/.bin/json -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.autoAssembly.enable=true'
 
 docker exec -it doc-linux sed -i 's/WARN/ALL/g' /etc/onlyoffice/documentserver/log4js/production.json
-docker exec -it doc-linux sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/onlyoffice-documentserver-example.conf
+docker exec -it doc-linux sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
 docker exec -it doc-linux supervisorctl restart all
 docker cp archive-share-libs.sh doc-linux:/tmp/archive-share-libs.sh
 docker exec -it doc-linux bash /tmp/archive-share-libs.sh

56bf01575d9456e3ce3b78e7c55ff49c7df1700b
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-09-27 15:19:36 +0300

    Remove arhive of libs after service start (#23)
    @flaminestone said he not use it any more,
    so it's safe to remove this feature

--- after-run.sh
+++ after-run.sh
@@ -7,6 +7,4 @@ docker exec -it doc-linux /var/www/onlyoffice/documentserver/npm/node_modules/.b
 docker exec -it doc-linux sed -i 's/WARN/ALL/g' /etc/onlyoffice/documentserver/log4js/production.json
 docker exec -it doc-linux sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
 docker exec -it doc-linux supervisorctl restart all
-docker cp archive-share-libs.sh doc-linux:/tmp/archive-share-libs.sh
-docker exec -it doc-linux bash /tmp/archive-share-libs.sh
 docker exec -it doc-linux dpkg-query --showformat='${Version}\n' --show onlyoffice-documentserver-ie

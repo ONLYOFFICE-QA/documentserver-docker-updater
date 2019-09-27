d053af28409c90583f9cfb85fccb29ea6a1f346a
Pavel Lobashov <shockwavenn@gmail.com>
2019-09-27 15:18:18 +0300

    Remove arhive of libs after service start
    @flaminestone said he not use it any more,
    so it's safe to remove this feature

--- archive-share-libs.sh
+++
@@ -1,2 +0,0 @@
-tar -cvf /var/www/onlyoffice/documentserver-example/public/libs.tar --directory="/var/www/onlyoffice/documentserver/server/FileConverter/bin" .
-echo "Show all libs and x2t in doc-linux.teamlab.info/example/libs.tar"

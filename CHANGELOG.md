262fa8e6bafc43afd32c3d8ab724ce50c5e39647
Pavel Lobashov <shockwavenn@gmail.com>
2020-08-22 16:58:22 +0300

    Change container name to more generic `DocumentServer`

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+
+## master (unreleased)
+
+* Change container name to more generic `DocumentServer`
--- after-run.sh
+++ after-run.sh
@@ -2,12 +2,12 @@ echo 'Sleep for 30 seconds to wait until documentserver reinitialize himself'
 sleep 30
 JSON_EXE=/var/www/onlyoffice/documentserver/npm/json
 
-docker exec -it doc-linux $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.expire.sessionidle="1h"'
-docker exec -it doc-linux $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.autoAssembly.enable=true'
+docker exec -it DocumentServer $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.expire.sessionidle="1h"'
+docker exec -it DocumentServer $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.autoAssembly.enable=true'
 
-docker exec -it doc-linux sed -i 's/WARN/ALL/g' /etc/onlyoffice/documentserver/log4js/production.json
-docker exec -it doc-linux sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
-docker exec -it doc-linux sed -i 's,access_log off,access_log /var/log/onlyoffice/documentserver/nginx.access.log,' /etc/onlyoffice/documentserver/nginx/includes/ds-common.conf
-docker exec -it doc-linux service nginx restart
-docker exec -it doc-linux supervisorctl restart all
-docker exec -it doc-linux dpkg-query --showformat='${Version}\n' --show onlyoffice-documentserver-ie
+docker exec -it DocumentServer sed -i 's/WARN/ALL/g' /etc/onlyoffice/documentserver/log4js/production.json
+docker exec -it DocumentServer sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
+docker exec -it DocumentServer sed -i 's,access_log off,access_log /var/log/onlyoffice/documentserver/nginx.access.log,' /etc/onlyoffice/documentserver/nginx/includes/ds-common.conf
+docker exec -it DocumentServer service nginx restart
+docker exec -it DocumentServer supervisorctl restart all
+docker exec -it DocumentServer dpkg-query --showformat='${Version}\n' --show onlyoffice-documentserver-ie

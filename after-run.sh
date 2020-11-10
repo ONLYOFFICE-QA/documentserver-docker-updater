7dd2b392fe6a9d9f9ddf154e9fb1c8a29aa2ff6a
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-11-10 18:33:41 +0300

    Fix incorrect variable name

--- after-run.sh
+++ after-run.sh
@@ -1,4 +1,4 @@
-PACKAGE-TYPE='onlyoffice-documentserver-ee'
+PACKAGE_TYPE='onlyoffice-documentserver-ee'
 echo 'Sleep for 30 seconds to wait until documentserver reinitialize himself'
 sleep 30
 JSON_EXE=/var/www/onlyoffice/documentserver/npm/json
@@ -11,4 +11,4 @@ docker exec -it DocumentServer sed 's,autostart=false,autostart=true,' -i /etc/s
 docker exec -it DocumentServer sed -i 's,access_log off,access_log /var/log/onlyoffice/documentserver/nginx.access.log,' /etc/onlyoffice/documentserver/nginx/includes/ds-common.conf
 docker exec -it DocumentServer service nginx restart
 docker exec -it DocumentServer supervisorctl restart all
-docker exec -it DocumentServer dpkg-query --showformat='${Version}\n' --show $PACKAGE-TYPE
+docker exec -it DocumentServer dpkg-query --showformat='${Version}\n' --show $PACKAGE_TYPE

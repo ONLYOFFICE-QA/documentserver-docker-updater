a9f3ac9952d05db9eb1bd6084d10de389aa93db2
Pavel Lobashov <shockwavenn@gmail.com>
2020-11-12 12:47:13 +0300

    Add ability to install some custom plugins

--- after-run.sh
+++ after-run.sh
@@ -9,6 +9,7 @@ docker exec -it DocumentServer $JSON_EXE -f /etc/onlyoffice/documentserver/defau
 docker exec -it DocumentServer sed -i 's/WARN/ALL/g' /etc/onlyoffice/documentserver/log4js/production.json
 docker exec -it DocumentServer sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
 docker exec -it DocumentServer sed -i 's,access_log off,access_log /var/log/onlyoffice/documentserver/nginx.access.log,' /etc/onlyoffice/documentserver/nginx/includes/ds-common.conf
+bash plugins-install.sh
 docker exec -it DocumentServer service nginx restart
 docker exec -it DocumentServer supervisorctl restart all
 docker exec -it DocumentServer dpkg-query --showformat='${Version}\n' --show $PACKAGE_TYPE

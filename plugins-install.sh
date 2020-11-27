fcae40424f3234b06516a91f7c121860dae7a95f
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-11-28 00:26:37 +0300

    Add ability to install some custom plugins (#39)

---
+++ plugins-install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DS_PLUGINS_DIR='/var/www/onlyoffice/documentserver/sdkjs-plugins'
+PLUGINS_REPO='sdkjs-plugins'
+TMP_PLUGINS_DIR=/tmp/$PLUGINS_REPO
+allowed_plugins=(example_count_words_and_characters)
+
+apt-get -y update
+apt-get -y install git
+git clone -b develop --depth 1 https://github.com/ONLYOFFICE/$PLUGINS_REPO/ $TMP_PLUGINS_DIR
+
+for i in "${allowed_plugins[@]}"
+do 
+    cp -r --verbose $TMP_PLUGINS_DIR/"$i" $DS_PLUGINS_DIR
+done
+rm -rf $TMP_PLUGINS_DIR

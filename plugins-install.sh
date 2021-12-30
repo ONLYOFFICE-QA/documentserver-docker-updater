54f3143cca8545ea05f0aa1df7ec59f4037895d7
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-12-30 16:27:14 +0300

    Use plugins from develop (#51)

--- plugins-install.sh
+++ plugins-install.sh
@@ -7,7 +7,7 @@ allowed_plugins=(example_controls)
 
 apt-get -y update
 apt-get -y install git
-git clone -b develop --depth 1 https://github.com/ONLYOFFICE/$PLUGINS_REPO/ $TMP_PLUGINS_DIR
+git clone -b master --depth 1 https://github.com/ONLYOFFICE/$PLUGINS_REPO/ $TMP_PLUGINS_DIR
 
 for i in "${allowed_plugins[@]}"
 do 
@@ -18,7 +18,7 @@ rm -rf $TMP_PLUGINS_DIR
 own_repos_plugins=(plugin-html plugin-telegram plugin-wordscounter plugin-typograf plugin-rainbow)
 for i in "${own_repos_plugins[@]}"
 do 
-    git clone -b develop --depth 1 https://github.com/ONLYOFFICE/"$i" /tmp/"$i"
+    git clone -b master --depth 1 https://github.com/ONLYOFFICE/"$i" /tmp/"$i"
     cp -r --verbose /tmp/"$i" $DS_PLUGINS_DIR
     rm -rfv /tmp/"$i"
 done

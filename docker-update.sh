cb30721b4982db6dfa5905366f644fca8b8389a1
Pavel Lobashov <shockwavenn@gmail.com>
2022-02-10 14:59:53 +0300

    Fix shellcheck issues

--- docker-update.sh
+++ docker-update.sh
@@ -8,17 +8,17 @@ fi
 date_time=$(date +"%Y-%m-%d-%H-%M")
 log_folder=/var/log/onlyoffice/documentserver/${date_time}/log
 files_folder=/var/log/onlyoffice/documentserver/${date_time}/files
-mkdir -pv ${log_folder}
-mkdir -pv ${files_folder}
-chmod -R 777 ${log_folder}
-chmod -R 777 ${files_folder}
+mkdir -pv "${log_folder}"
+mkdir -pv "${files_folder}"
+chmod -R 777 "${log_folder}"
+chmod -R 777 "${files_folder}"
 bash stop-service.sh
 docker run -i -t -d -p 80:80 -p 443:443 --name DocumentServer \
  --restart=always \
  -v /usr/share/fonts:/usr/share/fonts/custom \
  -v /opt/onlyoffice/Data:/var/www/onlyoffice/Data \
- -v ${log_folder}:/var/log/onlyoffice/documentserver \
- -v ${files_folder}:/var/lib/onlyoffice/documentserver/App_Data/cache/files/error \
+ -v "${log_folder}":/var/log/onlyoffice/documentserver \
+ -v "${files_folder}":/var/lib/onlyoffice/documentserver/App_Data/cache/files/error \
  -e JWT_ENABLED=true \
  -e JWT_SECRET=doc-linux \
  -e JWT_HEADER=AuthorizationJwt \

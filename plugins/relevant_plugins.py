389e397a9e68284a20eb858839aeada6fcdd147e
MaxSorokin <askonev007@gmail.com>
2023-10-03 19:52:15 +0300

    ref: pretty json

--- plugins/relevant_plugins.py
+++ plugins/relevant_plugins.py
@@ -15,15 +15,17 @@ if response.status_code == 200:
     json_pl = json.loads(response.text)
     json_pl = json_pl['payload']['tree']['items']
 
-    plugin_list = []
+    plugin_names = []
     for dict in json_pl:
-        plugin_list.append(dict['name'])
+        plugin_names.append(dict['name'])
 
-    print(json.dumps(plugin_list))
+    pretty_json = json.dumps(plugin_names, indent=4)
+
+    print(pretty_json)
 
     # Write JSON to a file
     with open('plugins-list-actual.json', 'w') as file:
-        file.write(json.dumps(plugin_list))
+        file.write(pretty_json)
 
 else:
     print(f"Failed to fetch page. Status code: {response.status_code}")

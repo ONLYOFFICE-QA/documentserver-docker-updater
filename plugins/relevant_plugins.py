1b7ec3f0efacd15fa3fbfbabafbc0c3e2f2a202d
MaxSorokin <askonev007@gmail.com>
2023-10-03 18:36:54 +0300

    feature: json containing current plug-ins & update py script

---
+++ plugins/relevant_plugins.py
@@ -0,0 +1,30 @@
+import json
+import requests
+
+MARKETPLACE = 'https://github.com/ONLYOFFICE/onlyoffice.github.io'
+BRANCH = 'tree/master'
+SDKJS_PLUGIN_SOURCE = 'sdkjs-plugins/content'
+
+URL = f"{MARKETPLACE}/{BRANCH}/{SDKJS_PLUGIN_SOURCE}"
+
+print(URL) 
+
+response = requests.request("GET", URL, headers={}, data={})
+
+if response.status_code == 200:
+    json_pl = json.loads(response.text)
+    json_pl = json_pl['payload']['tree']['items']
+
+    plugin_list = []
+    for dict in json_pl:
+        plugin_list.append(dict['name'])
+
+    print(json.dumps(plugin_list))
+    
+    # # Writing JSON to a file
+    with open('plugins-list-actual.json', 'w') as file:
+        file.write(json.dumps(plugin_list))
+
+    #     json.dump(plugin_list, file)
+else:
+    print(f"Failed to fetch page. Status code: {response.status_code}")

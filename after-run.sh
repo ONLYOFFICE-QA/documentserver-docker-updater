e908a59676c334f926ca56ce7bb90c254fd5c50d
MaxSorokin <askonev007@gmail.com>
2023-10-03 18:42:03 +0300

    feature: init pluginmaster.sh

--- after-run.sh
+++ after-run.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
+source pluginsmaster.sh
+
+
+SERNAME=DocumentServer
 PACKAGE_TYPE='onlyoffice-documentserver-ee'
-echo 'Sleep for 90 seconds to wait until documentserver reinitialize himself'
-sleep 90
 JSON_EXE=/var/www/onlyoffice/documentserver/npm/json
 
-docker exec -it DocumentServer $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.expire.sessionidle="1h"'
-docker exec -it DocumentServer $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.autoAssembly.enable=true'
-docker exec -it DocumentServer $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.server.tokenRequiredParams=false'
-docker exec -it DocumentServer $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.expire.files=3600'
-docker exec -it DocumentServer $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.FileConverter.converter.errorfiles="error"'
+echo 'Sleep for 90 seconds to wait until documentserver reinitialize himself'
+sleep 90
 
-docker exec -it DocumentServer sed -i 's/WARN/ALL/g' /etc/onlyoffice/documentserver/log4js/production.json
-docker exec -it DocumentServer sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
-docker exec -it DocumentServer sed -i 's,access_log off,access_log /var/log/onlyoffice/documentserver/nginx.access.log,' /etc/onlyoffice/documentserver/nginx/includes/ds-common.conf
-docker cp plugins-install.sh DocumentServer:/tmp
-docker exec -it DocumentServer bash /tmp/plugins-install.sh
-docker exec -it DocumentServer service nginx restart
-docker exec -it DocumentServer supervisorctl restart all
-docker exec -it DocumentServer dpkg-query --showformat='${Version}\n' --show $PACKAGE_TYPE
+docker exec -it $SERNAME $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.expire.sessionidle="1h"'
+docker exec -it $SERNAME $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.autoAssembly.enable=true'
+docker exec -it $SERNAME $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.server.tokenRequiredParams=false'
+docker exec -it $SERNAME $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.services.CoAuthoring.expire.files=3600'
+docker exec -it $SERNAME $JSON_EXE -f /etc/onlyoffice/documentserver/default.json -I -e 'this.FileConverter.converter.errorfiles="error"'
+
+docker exec -it $SERNAME sed -i 's/WARN/ALL/g' /etc/onlyoffice/documentserver/log4js/production.json
+docker exec -it $SERNAME sed 's,autostart=false,autostart=true,' -i /etc/supervisor/conf.d/ds-example.conf
+docker exec -it $SERNAME sed -i 's,access_log off,access_log /var/log/onlyoffice/documentserver/nginx.access.log,' /etc/onlyoffice/documentserver/nginx/includes/ds-common.conf
+_install_plugins $SERNAME
+docker exec -it $SERNAME dpkg-query --showformat='${Version}\n' --show $PACKAGE_TYPE

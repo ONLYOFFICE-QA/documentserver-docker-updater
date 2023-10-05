c95a54e9e2eec6633b31fd9e594d157f1cb6d927
MaxSorokin <askonev007@gmail.com>
2023-10-05 17:14:45 +0300

    ref: use py common param formatting

--- plugins/get_plugins_script.py
+++ plugins/get_plugins_script.py
@@ -4,15 +4,15 @@ from typing import List, Dict
 from os import makedirs
 from os.path import join, dirname, realpath, isdir
 
+REALPATH = dirname(realpath(__file__))
+
 MARKETPLACE = 'https://github.com/ONLYOFFICE/onlyoffice.github.io'
 BRANCH = 'tree/master'
 SDKJS_PLUGIN_SOURCE = 'sdkjs-plugins/content'
 
 URL = f"{MARKETPLACE}/{BRANCH}/{SDKJS_PLUGIN_SOURCE}"
-PLUGIN_LIST_PATH = join(dirname(realpath(__file__)),
-                        'plugins-list-actual.json')
-EXCLUDED_LIST_PATH = join(dirname(realpath(__file__)),
-                          'excluded-plugins-list.json')
+PLUGIN_LIST_PATH = join(REALPATH, 'plugins-list-actual.json')
+EXCLUDED_LIST_PATH = join(REALPATH, 'excluded-plugins-list.json')
 
 
 def read_json(path: str) -> List | Dict:
@@ -20,10 +20,12 @@ def read_json(path: str) -> List | Dict:
         return json.load(file)
 
 
-def write_json(path: str,
-               data: 'dict | list',
-               exclude: 'dict | list',
-               mode: str = 'w') -> None:
+def write_json(
+        path: str,
+        data: 'dict | list',
+        exclude: 'dict | list',
+        mode: str = 'w'
+) -> None:
     makedirs(dirname(path)) if not isdir(dirname(path)) else ...
     filtered_data = [el for el in data if el not in exclude]
     with open(path, mode) as file:
@@ -40,6 +42,8 @@ def get_plugins() -> list:
 
 if __name__ == "__main__":
     print(URL)
-    write_json(PLUGIN_LIST_PATH,
-               get_plugins(),
-               read_json(EXCLUDED_LIST_PATH))
+    write_json(
+        PLUGIN_LIST_PATH,
+        get_plugins(),
+        read_json(EXCLUDED_LIST_PATH)
+    )

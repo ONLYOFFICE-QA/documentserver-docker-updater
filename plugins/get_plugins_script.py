96053dc49f207c6ea974255eaf1ddcfd81f3fcac
Sorokin Maksim <60688343+askonev@users.noreply.github.com>
2023-10-05 18:57:13 +0300

    feature: Exclude plugins via json (#62)
    * feature: Exclude plugins via json
    
    * ref: use py common param formatting
    
    * ref: min ref

--- plugins/get_plugins_script.py
+++ plugins/get_plugins_script.py
@@ -1,25 +1,37 @@
 import json
 import requests
+from typing import List, Dict
 from os import makedirs
 from os.path import join, dirname, realpath, isdir
+
+REALPATH = dirname(realpath(__file__))
 
 MARKETPLACE = 'https://github.com/ONLYOFFICE/onlyoffice.github.io'
 BRANCH = 'tree/master'
 SDKJS_PLUGIN_SOURCE = 'sdkjs-plugins/content'
 
 URL = f"{MARKETPLACE}/{BRANCH}/{SDKJS_PLUGIN_SOURCE}"
-PLUGIN_LIST_PATH = join(dirname(realpath(__file__)),
-                        'plugins-list-actual.json')
+PLUGIN_LIST_PATH = join(REALPATH, 'plugins-list-actual.json')
+EXCLUDED_LIST_PATH = join(REALPATH, 'excluded-plugins-list.json')
 
 
-def write_json(path: str, data: 'dict | list', mode: str = 'w') -> None:
-    makedirs(dirname(path)) if not isdir(dirname(path)) else ...
+def read_json(path: str) -> List or Dict:
+    with open(path, "r") as file:
+        return json.load(file)
+
+
+def write_json(
+        path: str,
+        data: List or Dict,
+        mode: str = 'w'
+) -> None:
+    makedirs(dirname(path)) if not isdir(dirname(path)) else None
     with open(path, mode) as file:
         json.dump(data, file, indent=4)
         file.write("\n")
 
 
-def get_plugins() -> list:
+def get_plugins() -> List or Dict:
     with requests.get(URL) as response:
         response.raise_for_status()
         return [item['name'] for item in
@@ -28,4 +40,6 @@ def get_plugins() -> list:
 
 if __name__ == "__main__":
     print(URL)
-    write_json(PLUGIN_LIST_PATH, get_plugins())
+    filtered_data = [el for el in get_plugins() if
+                     el not in read_json(EXCLUDED_LIST_PATH)]
+    write_json(PLUGIN_LIST_PATH, filtered_data)

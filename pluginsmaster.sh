04add12c988ac108271d9aabbb39a0fe44f8a3f7
MaxSorokin <askonev007@gmail.com>
2024-10-08 17:01:51 +0300

    update: actual plugin list

--- pluginsmaster.sh
+++ pluginsmaster.sh
@@ -6,7 +6,7 @@ SDKJS_PLUGINS=/var/www/onlyoffice/documentserver/sdkjs-plugins
 
 _install_plugins() {
     SERNAME="$1"
-    docker cp ./plugins/"$ACTUAL_JSON" "$SERNAME":"$TOOLS_DIR"
-    docker exec "$SERNAME" cat "$TOOLS_DIR/$ACTUAL_JSON"
+    docker cp ./plugins/"$ACTUAL_JSON" "$SERNAME":"$SDKJS_PLUGINS"
+    docker exec "$SERNAME" cat "$SDKJS_PLUGINS/$ACTUAL_JSON"
     docker exec "$SERNAME" $SCRIPT --install="$SDKJS_PLUGINS/$ACTUAL_JSON"
 }
